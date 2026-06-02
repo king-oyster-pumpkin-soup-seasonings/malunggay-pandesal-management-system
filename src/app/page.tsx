8dd0c355949fe5d3a9cb7f2cd461f4f4d1e8b15d
king-oyster-pumpkin-soup-seasonings <283332318+king-oyster-pumpkin-soup-seasonings@users.noreply.github.com>
2026-06-02 09:02:57 +0800

    chore: add rolling malunggay pandesal image and update layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+    <div className="flex flex-col flex-1 items-center justify-center bg-green-300 font-sans dark:bg-black">
+      <main className="flex max-w-3xl w-full flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="mx-auto">MALUNGGAY PANDESAL</h1>
+        <p>それ、一体どんな絵なの！</p>
+        <p>1023123 javascript</p>
+      </main>
     </div>
   );
 }
